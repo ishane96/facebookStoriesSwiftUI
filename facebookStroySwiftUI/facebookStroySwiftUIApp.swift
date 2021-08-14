060d8a8c6cfafafb73e03a6f6a06c856221ebd6c
Achintha Kahawalage <achintha.emedia@gmail.com>
2021-08-15 01:15:16 +0530

    initial commit

---
+++ facebookStroySwiftUI/facebookStroySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  facebookStroySwiftUIApp.swift
+//  facebookStroySwiftUI
+//
+//  Created by Achintha Kahawalage on 2021-07-05.
+//
+
+import SwiftUI
+
+@main
+struct facebookStroySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
